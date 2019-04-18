4200684fec33da86b4a37bd9089d3b44af693670
Matthew Woehlke <matthew.woehlke@kitware.com>
2019-04-18 13:48:28 -0400

    Improve AutoLevelsTask unit test
    Tweak the unit test for AutoLevelsTask to validate every intermediate
    result offered by the task, not just the final result.

--- sealtk/core/test/AutoLevelsTask.cpp
+++ sealtk/core/test/AutoLevelsTask.cpp
@@ -8,6 +8,7 @@
 
 #include <vital/algo/image_io.h>
 #include <vital/plugin_loader/plugin_manager.h>
+#include <vital/range/iota.h>
 
 #include <qtStlUtil.h>
 
@@ -15,6 +16,21 @@
 #include <QtTest>
 
 namespace kv = kwiver::vital;
+namespace kvr = kwiver::vital::range;
+
+namespace // anonymous
+{
+
+// ============================================================================
+struct LevelPair
+{
+  float low;
+  float high;
+};
+
+} // namespace <anonymous>
+
+Q_DECLARE_METATYPE(LevelPair)
 
 namespace sealtk
 {
@@ -59,27 +75,25 @@ void TestAutoLevelsTask::levels()
   // Fetch test case parameters
   QFETCH(double, outlierDeviance);
   QFETCH(double, outlierTolerance);
-  QFETCH(float, expectedLow);
-  QFETCH(float, expectedHigh);
+  QFETCH(QVector<LevelPair>, expected);
 
   // Set up the task
-  auto computedLow = 0.0f;
-  auto computedHigh = 1.0f;
+  QVector<LevelPair> computed;
   AutoLevelsTask task{m_image, outlierDeviance, outlierTolerance};
 
   connect(&task, &AutoLevelsTask::levelsUpdated,
-          [&](float l, float h){
-            qDebug() << "result:" << l << h;
-            computedLow = l;
-            computedHigh = h;
-          });
+          [&](float l, float h){ computed.append({l, h}); });
 
   // Run the task
   task.execute();
 
   // Compare computed result to expected result
-  QCOMPARE(computedLow, expectedLow);
-  QCOMPARE(computedHigh, expectedHigh);
+  QCOMPARE(computed.count(), expected.count());
+  for (auto i : kvr::iota(expected.count()))
+  {
+    QCOMPARE(computed[i].low, expected[i].low);
+    QCOMPARE(computed[i].high, expected[i].high);
+  }
 }
 
 // ----------------------------------------------------------------------------
@@ -87,19 +101,43 @@ void TestAutoLevelsTask::levels_data()
 {
   QTest::addColumn<double>("outlierDeviance");
   QTest::addColumn<double>("outlierTolerance");
-  QTest::addColumn<float>("expectedLow");
-  QTest::addColumn<float>("expectedHigh");
+  QTest::addColumn<QVector<LevelPair>>("expected");
 
-  QTest::newRow("strict")
-    << 0.0 << 0.0 << 0.30859375f << 1.0f;
-  QTest::newRow("d=5%, t=0")
-    << 0.05 << 0.00 << 0.427734375f << 0.568359375f;
-  QTest::newRow("d=2%, t=0")
-    << 0.02 << 0.00 << 0.41015625f << 0.5849609375f;
-  QTest::newRow("d=2%, t=20%")
-    << 0.02 << 0.20 << 0.41015625f << 0.5849609375f;
-  QTest::newRow("d=2%, t=60%")
-    << 0.02 << 0.60 << 0.30859375f << 0.5849609375f;
+  QTest::newRow("strict") << 0.0 << 0.0 << QVector<LevelPair>{
+    {0.3642578125f, 0.630859375f},
+    {0.3291015625f, 0.7900390625f},
+    {0.3291015625f, 1.0f},
+    {0.30859375f, 1.0f},
+    {0.30859375f, 1.0f},
+  };
+  QTest::newRow("d=5%, t=0") << 0.05 << 0.00 << QVector<LevelPair>{
+    {0.4248046875f, 0.5703125f},
+    {0.427734375f, 0.568359375f},
+    {0.427734375f, 0.568359375f},
+    {0.427734375f, 0.568359375f},
+    {0.427734375f, 0.568359375f},
+  };
+  QTest::newRow("d=2%, t=0") << 0.02 << 0.00 << QVector<LevelPair>{
+    {0.4091796875f, 0.583984375f},
+    {0.41015625f, 0.583984375f},
+    {0.41015625f, 0.5849609375f},
+    {0.41015625f, 0.5849609375f},
+    {0.41015625f, 0.5849609375f},
+  };
+  QTest::newRow("d=2%, t=20%") << 0.02 << 0.20 << QVector<LevelPair>{
+    {0.4091796875f, 0.583984375f},
+    {0.41015625f, 0.583984375f},
+    {0.41015625f, 0.5849609375f},
+    {0.41015625f, 0.5849609375f},
+    {0.41015625f, 0.5849609375f},
+  };
+  QTest::newRow("d=2%, t=60%") << 0.02 << 0.60 << QVector<LevelPair>{
+    {0.3642578125f, 0.630859375f},
+    {0.3291015625f, 0.583984375f},
+    {0.3291015625f, 0.5849609375f},
+    {0.30859375f, 0.5849609375f},
+    {0.30859375f, 0.5849609375f},
+  };
 }
 
 } // namespace test
