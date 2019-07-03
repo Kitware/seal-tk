63c94442d7aefb9638a3abb8ab52e9a4558ec520
Matthew Woehlke <matthew.woehlke@kitware.com>
2019-07-03 14:26:50 -0400

    Add FusionModel
    Create a new model which produces a fusion of items from multiple
    models. For now, this only supports immutable models, and only a fairly
    small subset of possible model data, but this should be adequate for our
    immediate needs.

---
+++ sealtk/gui/FusionModel.hpp
@@ -0,0 +1,49 @@
+/* This file is part of SEAL-TK, and is distributed under the OSI-approved BSD
+ * 3-Clause License. See top-level LICENSE file or
+ * https://github.com/Kitware/seal-tk/blob/master/LICENSE for details. */
+
+#ifndef sealtk_gui_FusionModel_hpp
+#define sealtk_gui_FusionModel_hpp
+
+#include <sealtk/gui/Export.h>
+
+#include <sealtk/core/AbstractItemModel.hpp>
+
+#include <qtGlobal.h>
+
+namespace sealtk
+{
+
+namespace gui
+{
+
+class FusionModelPrivate;
+
+class SEALTK_GUI_EXPORT FusionModel : public sealtk::core::AbstractItemModel
+{
+  Q_OBJECT
+
+public:
+  FusionModel(QObject* parent = nullptr);
+  ~FusionModel();
+
+  // Reimplemented from QAbstractItemModel
+  int rowCount(QModelIndex const& parent = {}) const override;
+  QVariant data(QModelIndex const& index, int role) const override;
+
+public slots:
+  void addModel(QAbstractItemModel*);
+  void removeModel(QAbstractItemModel*);
+
+protected:
+  QTE_DECLARE_PRIVATE_PTR(FusionModel);
+
+private:
+  QTE_DECLARE_PRIVATE(FusionModel);
+};
+
+} // namespace gui
+
+} // namespace sealtk
+
+#endif
