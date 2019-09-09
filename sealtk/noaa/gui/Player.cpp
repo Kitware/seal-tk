e433f790a5cbe6530ad6dfdb26591fd6bdc1f18c
Matt Dawkins <matt.dawkins@kitware.com>
2019-09-09 00:53:33 -0400

    Don't hard-code ITK plugin for transforms
    Use the "auto" plugin — which has the ability to automatically detect the
    file type of a transform file — to load transforms, rather than
    hard-coding the "itk" plugin.

--- sealtk/noaa/gui/Player.cpp
+++ sealtk/noaa/gui/Player.cpp
@@ -141,7 +141,7 @@ void PlayerPrivate::loadTransform(Player* q)
   if (!path.isEmpty())
   {
     auto config = kv::config_block::empty_config();
-    config->set_value("transform_reader:type", "itk");
+    config->set_value("transform_reader:type", "auto");
 
     try
     {
