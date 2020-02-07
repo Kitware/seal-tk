34bd21079bd3c4de02dabb914fc2556a3ad210b1
Matthew Woehlke <matthew.woehlke@kitware.com>
2020-02-07 13:40:10 -0500

    Refactor Sink test
    Extract the utility model and VideoSource from the KwiverDetectionsSink
    unit test so that they can be reused. Both of these will also be useful
    for creating a unit test for the recently added KwiverTracksSink. (The
    compareFiles would also be useful, but it's short and has a bunch of
    dependencies, making it not really worth the effort.)

---
+++ sealtk/core/test/TestTrackModel.cpp
@@ -0,0 +1,95 @@
+/* This file is part of SEAL-TK, and is distributed under the OSI-approved BSD
+ * 3-Clause License. See top-level LICENSE file or
+ * https://github.com/Kitware/seal-tk/blob/master/LICENSE for details. */
+
+#include <sealtk/core/test/TestTrackModel.hpp>
+
+#include <sealtk/core/DataModelTypes.hpp>
+
+namespace sealtk
+{
+
+namespace core
+{
+
+namespace test
+{
+
+// ----------------------------------------------------------------------------
+SimpleTrackModel::SimpleTrackModel(QVector<TimeMap<TrackState>> data)
+  : rowData{data}
+{
+}
+
+// ----------------------------------------------------------------------------
+int SimpleTrackModel::rowCount(QModelIndex const& parent) const
+{
+  if (parent.isValid())
+  {
+    auto const pr = parent.row();
+    if (pr >= 0 && pr < this->rowData.count())
+    {
+      return this->rowData[pr].count();
+    }
+    return 0;
+  }
+  else
+  {
+    return this->rowData.count();
+  }
+}
+
+// ----------------------------------------------------------------------------
+QModelIndex SimpleTrackModel::index(
+  int row, int column, QModelIndex const& parent) const
+{
+  auto const pr =
+    static_cast<unsigned>(parent.isValid() ? parent.row() + 1 : 0);
+
+  return this->createIndex(row, column, pr);
+}
+
+// ----------------------------------------------------------------------------
+QModelIndex SimpleTrackModel::parent(QModelIndex const& index) const
+{
+  auto const parentRow = static_cast<int>(index.internalId()) - 1;
+  return (parentRow >= 0 ? this->createIndex(parentRow, 0) : QModelIndex{});
+}
+
+// ----------------------------------------------------------------------------
+QVariant SimpleTrackModel::data(QModelIndex const& index, int role) const
+{
+  if (this->checkIndex(index, IndexIsValid))
+  {
+    auto const& p = index.parent();
+    if (p.isValid())
+    {
+      auto const pr = p.row();
+      auto const& track = this->rowData[pr];
+
+      switch (role)
+      {
+        case StartTimeRole:
+        case EndTimeRole:
+          return QVariant::fromValue(track.keys()[index.row()]);
+
+        case AreaLocationRole:
+          return track.values()[index.row()].location;
+
+        case ClassificationRole:
+          return track.values()[index.row()].classification;
+
+        default:
+          break;
+      }
+    }
+  }
+
+  return AbstractItemModel::data(index, role);
+}
+
+} // namespace test
+
+} // namespace core
+
+} // namespace sealtk
